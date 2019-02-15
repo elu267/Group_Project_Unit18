7d02fe88715f7a94bd8cfd6b46a040cc84f71e28
rwang0515 <RWANG0515@GMAIL.COM>
2019-02-15 16:39:18 -0600

    Update plotly_Rui.js

--- skiResort/static/js/plotly_Rui.js
+++ skiResort/static/js/plotly_Rui.js
@@ -9,6 +9,8 @@ d3.csv('clean_skiResortsNA.csv').then(function(data) {
     const sizes = data.map((x) => { return x.ResortSize });
     const adult_price = data.map((x) => { return x.Adult });
     const lat = data.map((x) => { return x.Latitude });
+    const long = data.map((x) => {return x.Longitude});
+    const easy = data.map((x) => {return x.Easy});
     // const canada_
     // id vs size
     // Build a Bubble Chart
@@ -66,13 +68,49 @@ d3.csv('clean_skiResortsNA.csv').then(function(data) {
             colorscale: "Earth"
         }
     }];
-
+    var bubbleLayout_4 = {
+        margin: { t: 0 },
+        hovermode: "closest",
+        xaxis: { title: "Longitude" },
+        yaxis: { title: "Easy Slope" }
+    };
+    var bubbleData_4 = [{
+        x: long,
+        y: easy,
+        text: names,
+        mode: "markers",
+        marker: {
+            size: long,
+            color: long,
+            colorscale: "Earth"
+        }
+    }];
+    
+    var bubbleLayout_5 = {
+        margin: { t: 0 },
+        hovermode: "closest",
+        xaxis: { title: "Longtitude" },
+        yaxis: { title: "Latitude" }
+    };
+    var bubbleData_5 = [{
+        x: long,
+        y: lat,
+        text: names,
+        mode: "markers",
+        // marker: {
+           // size: lat,
+           // color: sizes,
+           // colorscale: "Earth"
+        // }
+    }];
+    
+    
     Plotly.plot("scatter", bubbleData_1, bubbleLayout_1);
 
-    let button = document.getElementById("toggle");
-    let toggle = 1;
+    let button = document.getElementById("toggle_1");
+    let toggle_1 = 1;
     button.addEventListener("click", () => {
-        switch (++toggle % 3) {
+        switch (++toggle_1 % 5) {
             case 0:
                 Plotly.purge("scatter");
                 Plotly.plot("scatter", bubbleData_1, bubbleLayout_1);
@@ -83,7 +121,15 @@ d3.csv('clean_skiResortsNA.csv').then(function(data) {
                 break;
             case 2:
                 Plotly.purge("scatter");
-                Plotly.plot("scatter", bubbleData_3, bubbleLayout_3);
+                Plotly.plot("scatter", bubbleData_3, bubbleLayout_3);  
+                break;
+            case 3:
+                Plotly.purge("scatter");
+                Plotly.plot("scatter", bubbleData_4, bubbleLayout_4);
+                break;
+            case 4:
+                Plotly.purge("scatter");
+                Plotly.plot("scatter", bubbleData_5, bubbleLayout_5);
                 break;
         }
     })
