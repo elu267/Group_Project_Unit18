22bcfb6e03634548b46ca893b9430e2c228a2a5f
rwang0515 <RWANG0515@GMAIL.COM>
2019-02-16 08:38:45 -0600

    Update plotly_Rui.js

--- skiResort/static/js/plotly_Rui.js
+++ skiResort/static/js/plotly_Rui.js
@@ -2,19 +2,42 @@ d3.json('/resorts').then(function(data) {
 
     // x is each object of data
     const ids = data.map((x) => {
-        // console.log(y + " ---- " + JSON.stringify(x));
+        // console.log("asdfds");
         return x.ID;
     });
     const names = data.map((x) => { return x.ResortName });
     const sizes = data.map((x) => { return x.ResortSize });
+    
     const adult_price = data.map((x) => { return x.Adult });
+    
     const lat = data.map((x) => { return x.Latitude });
     const long = data.map((x) => { return x.Longitude });
     const easy = data.map((x) => { return x.Easy });
-    const state_count = [_.countBy(data.StateProvince, data.StateProvince).head(20)];
-    const country_count = [_.countyBy(data.Country, data.Country)];
-    const altitude = data.map((x) => { return x.Altitude });
 
+    // const state_count = [countBy(data.StateProvince, data.StateProvince).head(20)];
+    var state = data.map(x => { return x.StateProvince});
+    var state_counts ={};
+    for(var i = 0; i < state.length; i++){state_counts[state[i]]=1+(state_counts[state[i]] || 0)};
+
+
+    // // var state_key=[]
+    var state_counts_output = Object.entries(state_counts).map(([key, value])=>({key,value}));
+    var state_counts_output = state_counts_output.slice(0,14);
+    var state_counts_output_key = state_counts_output.map((x)=>{return x.key});
+    var state_counts_output_value = state_counts_output.map((x)=>{return x.value});
+
+    var country = data.map(x => { return x.Country});
+    var country_counts ={};
+    for(var i = 0; i < country.length; i++){country_counts[country[i]]=1+(country_counts[country[i]] || 0)};
+
+
+    // // var state_key=[]
+    var country_counts_output = Object.entries(country_counts).map(([key, value])=>({key,value}));
+    var country_counts_output_key = country_counts_output.map((x)=>{return x.key});
+    var country_counts_output_value = country_counts_output.map((x)=>{return x.value});
+
+    const altitude = data.map((x) => { return x.Altitude });
+    
     // const canada_
     // id vs size
     // Build a Bubble Chart
@@ -30,11 +53,12 @@ d3.json('/resorts').then(function(data) {
         text: names,
         mode: "markers",
         marker: {
-            size: adult_price ** 3,
+            size: adult_price ** 0.2,
             color: altitude,
             colorscale: "Earth"
         }
     }];
+
     // size vs price
     var bubbleLayout_2 = {
         margin: { t: 0 },
@@ -54,7 +78,7 @@ d3.json('/resorts').then(function(data) {
         }
     }];
 
-    // // bar chart
+   
     var bubbleLayout_3 = {
         margin: { t: 0 },
         hovermode: "closest",
@@ -84,9 +108,9 @@ d3.json('/resorts').then(function(data) {
         text: names,
         mode: "markers",
         marker: {
-            size: long,
-            color: long,
-            colorscale: "Earth"
+            size: easy
+            // color: easy,
+            // colorscale: "Earth"
         }
     }];
 
@@ -108,25 +132,24 @@ d3.json('/resorts').then(function(data) {
         // }
     }];
     var barLayout_1 = {
-        margin: { t: 0 },
-        hovermode: "closest",
-        xaxis: { title: "Number of ski resorts" },
-        yaxis: { title: "State" }
+        yaxis: { title: "Number of ski resorts" }
     };
+    
     var barData_1 = [{
-        x: state_count[0],
-        y: state_count[1]
+        x: state_counts_output_key,
+        y: state_counts_output_value,
+        type: "bar"
     }];
 
+
+
     var barLayout_2 = {
-        margin: { t: 0 },
-        hovermode: "closest",
-        xaxis: { title: "Number of ski resorts" },
-        yaxis: { title: "Country" }
+        yaxis: { title: "Number of ski resorts" }
     };
     var barData_2 = [{
-        x: country_count[0],
-        y: country_count[1]
+        x: country_counts_output_key,
+        y: country_counts_output_value,
+        type: bar
     }];
 
     Plotly.plot("scatter", bubbleData_1, bubbleLayout_1);
@@ -158,7 +181,7 @@ d3.json('/resorts').then(function(data) {
         }
     })
 
-    Plotly.plot("bar", barData_1, barLayout_1);
+    Plotly.plot("bar", barData_1);
 
     let button2 = document.getElementById("toggle_2");
     let toggle_2 = 1;
